82862b026fa826ec6b3154c9942ebbc4d28e3b8e
Zdenal <nevralaz@gmail.com>
2014-10-25 17:20:51 +0200

    Fix comparing hashes on debouncer

--- nodes/artemis/msgDebouncer.js
+++ nodes/artemis/msgDebouncer.js
@@ -1,11 +1,12 @@
 module.exports = function(RED) {
+    var _ = require('underscore')
 
     function MsgDebouncer(config) {
 
         RED.nodes.createNode(this,config);
 
         this.on('input', function(msg) {
-          if(msg.topic != this.topic || msg.payload != this.payload){
+          if(!_.isEqual(msg.topic, this.topic) || !_.isEqual(msg.payload, this.payload)){
             this.send(msg);
             this.topic = msg.topic;
             this.payload = msg.payload;
