de7b9c7510a7794365cfa360a67842f8c4e9681a
Vratislav Kalenda <v.kalenda@gmail.com>
2014-10-25 12:41:56 +0200

    Added artemis-client dependencies and stub implementation

---
+++ nodes/artemis/artemis-client.js
@@ -0,0 +1,11 @@
+module.exports = function(RED) {
+    function ArtemisClient(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        this.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("artemis-client",ArtemisClient);
+}
